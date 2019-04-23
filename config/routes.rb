aa5a076e15d0cb791dca13f7aed9f415f4a0291a
Ray Valdez <ray_valdez@hotmail.co.uk>
2019-04-23 19:40:58 +0100

    Add SessionsController with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get '/signup' => 'users#new'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

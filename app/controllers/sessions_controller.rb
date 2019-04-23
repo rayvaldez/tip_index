7c51a0ae84984f18354f238b1e7aa00a3c6f4723
Ray Valdez <ray_valdez@hotmail.co.uk>
2019-04-23 20:03:39 +0100

    Add actions to User and Session controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,15 @@
 class SessionsController < ActionController::Base
+
+  #load form for new session
+  def new
+  end
+
+  #create a new session from params
+  def create
+  end
+
+  #redirect to home after login
+  def home
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+
+  #load form for new user
+  def new
+  end
+
+  #create a user
+  def create
+  end
 end
